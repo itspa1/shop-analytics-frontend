4eff0fe3ef15ec967b7f6af665b52601ab60c383
Pavan Kumar <kumarpavan1309@gmail.com>
2020-06-03 10:36:41 +0530

    testing grafana iframe url

--- src/views/Iframe.js
+++ src/views/Iframe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Iframe from "react-iframe";
 
+import { iframeUrl } from "configs/env";
 import { Card, Row, Col } from "reactstrap";
 
 class IframeComponent extends React.Component {
@@ -37,8 +38,8 @@ class IframeComponent extends React.Component {
     // const url = `http://localhost:5000/d-solo/2q9G60RGk/new-dashboard-copy?orgId=1&from=${this.state.from}&to=${this.state.to}&refresh=${this.state.duration}&panelId=2`;
     // const url =
     //   "http://localhost:5000/d/2q9G60RGk/new-dashboard-copy?orgId=1&from=now-5m&to=now&refresh=5s";
-    const url =
-      "http://localhost:5000/dashboard/snapshot/cTP4g31FzXiXz3yeV2Zo6R58bEGMK4oQ";
+    const url = iframeUrl;
+    // "http://localhost:5000/dashboard/snapshot/cTP4g31FzXiXz3yeV2Zo6R58bEGMK4oQ";
     return (
       <div className="content">
         <Row>
